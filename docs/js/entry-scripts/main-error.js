91d367c690532c6da04cf1e4c47e190da2930035
Alvaro Arraras <85067108+Alvaro84060@users.noreply.github.com>
2025-06-29 06:57:23 +0200

    Delete docs directory

--- docs/js/entry-scripts/main-error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-require("../modules/common.js");
-require("../modules/aside-menu.js");
-require("../modules/theme-toggler.js");
-require("../modules/modal.js");
-require("../modules/location-modal.js");
-//# sourceMappingURL=main-error.js.map
